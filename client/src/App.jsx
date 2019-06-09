2bc938a573ec43a70987cbef0336e81bb4996cfb
mishapodl <mpodlevskykh@gmail.com>
2019-06-10 00:22:05 +0300

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
 			<div>
 				<AppNavbar />
 				<Container>
-					<ItemModal />
+					{/* <ItemModal /> */}
 					<ShoppingList />
 				</Container>
 			</div>
