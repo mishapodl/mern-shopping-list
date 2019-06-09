b6aea4bcbf5616fdeb868215772dc0cc612a8815
mishapodl <mpodlevskykh@gmail.com>
2019-06-10 01:07:34 +0300

    realy fixd heroku

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
 
 // Item Model
 const Item = require('../../models/Item');
@@ -17,7 +16,7 @@ router.get('/', (req, res) => {
 // @route   POST api/items
 // @desc    Create An Item
 // @access  Private
-router.post('/', auth, (req, res) => {
+router.post('/', (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
@@ -28,7 +27,7 @@ router.post('/', auth, (req, res) => {
 // @route   DELETE api/items/:id
 // @desc    Delete A Item
 // @access  Private
-router.delete('/:id', auth, (req, res) => {
+router.delete('/:id', (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
--- server.js
+++ server.js
@@ -1,38 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
-const config = require('config');
+
+const items = require('./routes/api/items')
 
 const app = express();
 
-// Bodyparser Middleware
-app.use(express.json());
+//Body
+app.use(bodyParser.json());
 
-// DB Config
-const db = config.get('mongoURI');
+//DB
+const uri = require('./config/keys').mongoURI;
 
-// Connect to Mongo
+//Connect
 mongoose
-.connect(db, { 
-	useNewUrlParser: true,
-	useCreateIndex: true
-}) // Adding new mongo url parser
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+.connect(uri, {useNewUrlParser: true})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.error(err));
 
-// Use Routes
 app.use('/api/items', require('./routes/api/items'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-// Set static folder
-app.use(express.static('client/build'));
+  // Set static folder
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
