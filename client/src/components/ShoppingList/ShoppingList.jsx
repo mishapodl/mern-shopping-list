96035f829ab7eed8aee056c531a53898fbb335d5
mishapodl <mpodlevskykh@gmail.com>
2019-06-28 19:31:42 +0300

    3 пробела

--- client/src/components/ShoppingList/ShoppingList.jsx
+++ client/src/components/ShoppingList/ShoppingList.jsx
@@ -12,54 +12,54 @@ import { connect } from 'react-redux'
 
 
 class ShoppingList extends Component {
-	static propTypes = {
-		getItems: PropTypes.func.isRequired,
-		item: PropTypes.object.isRequired,
-	 };
-	componentDidMount() {
-		this.props.getItems()
-	}
+   static propTypes = {
+      getItems: PropTypes.func.isRequired,
+      item: PropTypes.object.isRequired,
+    };
+   componentDidMount() {
+      this.props.getItems()
+   }
 
-	odHandlerDeleteItem = (id) => {
-		this.props.deleteItem(id)
-	}
+   odHandlerDeleteItem = (id) => {
+      this.props.deleteItem(id)
+   }
 
-	render() {
-		const { items } = this.props.items;
-		return (
-			<Container>
-				<ListGroup>
-					<TransitionGroup className="shopping-list">
-						{
-							items.map(({ _id, name }) => (
-								<CSSTransition key={_id} timeout={500} classNames="fade">
-									<ListGroupItem>
-										<Button
-											className="remove-btn"
-											color="danger"
-											size="sm"
-											onClick={this.odHandlerDeleteItem.bind(this, _id)}
-										>
-											&times;
-										</Button>
-										{ name }
-									</ListGroupItem>
-								</CSSTransition>
-							))
-						}
-					</TransitionGroup>
-				</ListGroup>
-			</Container>
-		);
-	}
+   render() {
+      const { items } = this.props.items;
+      return (
+         <Container>
+            <ListGroup>
+               <TransitionGroup className="shopping-list">
+                  {
+                     items.map(({ _id, name }) => (
+                        <CSSTransition key={_id} timeout={500} classNames="fade">
+                           <ListGroupItem>
+                              <Button
+                                 className="remove-btn"
+                                 color="danger"
+                                 size="sm"
+                                 onClick={this.odHandlerDeleteItem.bind(this, _id)}
+                              >
+                                 &times;
+                              </Button>
+                              { name }
+                           </ListGroupItem>
+                        </CSSTransition>
+                     ))
+                  }
+               </TransitionGroup>
+            </ListGroup>
+         </Container>
+      );
+   }
 };
 
 const mapStateToProps = state => ({
-	items: state.items
+   items: state.items
 })
 
 const mapDispatchToProps = {
-	getItems, deleteItem
+   getItems, deleteItem
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingList);
